8247ed2427056f163af8afbe9e617a493c228a99
Drake Semchyshyn <desemchyshyn@gmail.com>
2025-04-01 21:17:04 -0500

    v0.5.3a1d1 - canvas token example

--- src/app/(onboarding)/app/onboarding/canvas/page.client.tsx
+++ src/app/(onboarding)/app/onboarding/canvas/page.client.tsx
@@ -5,7 +5,8 @@ import { Combobox } from "@/components/ui/combobox";
 import { Input } from "@/components/ui/input";
 import { ArrowRight } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { Ref, RefObject, useEffect, useRef, useState } from "react";
+import Lottie, { ILottie } from '@lottielab/lottie-player/react';
 
 export default function OnboardingPageClient({
   tokenIsSaved,
@@ -16,9 +17,24 @@ export default function OnboardingPageClient({
 
   const [token, setToken] = useState<string>("");
 
+  const lottie = useRef(undefined) as unknown as RefObject<HTMLDivElement> | undefined;
+
+  let removed = 0;
+  const MAX_REMOVE = 1000;
+
+  const toRemove = setInterval(() => {
+    const el = lottie?.current?.querySelector("svg > g > g:has(g > g[opacity=\"0.8\"])");
+    el?.remove();
+
+    removed++;
+    if (removed >= MAX_REMOVE) {
+      clearInterval(toRemove);
+    }
+  }, 10);
+
   return (
     <div className="flex flex-col gap-2">
-      <div className="flex gap-2">
+      <div className="flex gap-2 flex-col">
         <label className="text-xs text-muted-foreground flex-1">
           Canvas Token
           <Input
@@ -28,6 +44,9 @@ export default function OnboardingPageClient({
             }}
           />
         </label>
+        <div className="bg-white render-white-content" ref={lottie}>
+          <Lottie src="/canvas.lottie.json" autoplay loop />
+        </div>
       </div>
       <div className="flex justify-end items-center gap-2 mt-4">
         <Button
